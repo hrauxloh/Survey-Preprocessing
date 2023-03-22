fac0bfe85797b468ef107d9846b60116af364269
hrauxloh <113611456+hrauxloh@users.noreply.github.com>
2023-03-22 15:01:18 +0100

    Update function__find_filter_duplicates.py

--- function__find_filter_duplicates.py
+++ function__find_filter_duplicates.py
@@ -10,8 +10,9 @@ import pandas as pd
 import numpy as np
 
 def find_filter_duplicates(df, id_column, date_column, filepath_question_metadata):
+  df[date_column] = df[date_column].dt.tz_convert(None)  
   df[date_column] = pd.to_datetime(df[date_column], format='%Y-%m-%d %H:%M:%S')
-  df[date_column] = df[date_column].dt.tz_convert(None)
+  
 #   if df[date_column].dtype != 'datetime64[ns, UTC]':
 #     return print("The function requires a date/time column with data in the format 'datetime64'.")
   
